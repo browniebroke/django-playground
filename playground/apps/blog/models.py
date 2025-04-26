46cf640e18a0bbddda4c82b7bc961472c802c341
Bruno Alla <alla.brunoo@gmail.com>
2025-04-26 13:57:52 +0100

    Play around with https://code.djangoproject.com/ticket/35813

--- playground/apps/blog/models.py
+++ playground/apps/blog/models.py
@@ -3,8 +3,12 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    published_date = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        managed = False
 
     def __str__(self):
         return self.title
