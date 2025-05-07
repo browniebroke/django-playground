c4356acc35b0de9e1d0bfa2d90e21235cc62f4b6
Bruno Alla <alla.brunoo@gmail.com>
2025-05-07 16:41:58 +0100

    Add django-extensions

--- playground/apps/blog/tests.py
+++ playground/apps/blog/tests.py
@@ -1,3 +1,7 @@
+from django.core.management import call_command
 from django.test import TestCase
 
-# Create your tests here.
+class TestExtensions(TestCase):
+    def test_list_signals(self):
+        result = call_command("list_signals")
+        self.assertEqual('', result)
