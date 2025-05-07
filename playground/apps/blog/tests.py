20adac18e5813d990e5d13754157beb581e6687f
Bruno Alla <alla.brunoo@gmail.com>
2025-05-07 16:43:04 +0100

    Add openai to reproduce the bug

--- playground/apps/blog/tests.py
+++ playground/apps/blog/tests.py
@@ -1,7 +1,9 @@
 from django.core.management import call_command
 from django.test import TestCase
+import openai
 
 class TestExtensions(TestCase):
     def test_list_signals(self):
+        assert openai is not None
         result = call_command("list_signals")
         self.assertEqual('', result)
