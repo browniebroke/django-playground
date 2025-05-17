7e0635e1bff3e24fe530c8112fefdeaac91e9d5b
Bruno Alla <alla.brunoo@gmail.com>
2025-05-17 11:47:33 +0100

    Add models without composite PK

--- playground/apps/blog/models.py
+++ playground/apps/blog/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Order(models.Model):
+    reference = models.CharField(max_length=20, primary_key=True)
+
+
+class OrderLineItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
