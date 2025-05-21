a63b67cc09b7b26726d0646facefcc76d2d1caa0
Bruno Alla <alla.brunoo@gmail.com>
2025-05-21 17:59:57 +0100

    URL validation discussion

--- playground/apps/blog/tests.py
+++ playground/apps/blog/tests.py
@@ -1,3 +1,18 @@
+from django.core.validators import URLValidator
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework import serializers
+
+class MySerializer(serializers.Serializer):
+    url = serializers.URLField()
+
+
+class TestUrl(TestCase):
+    def test_valid_url(self):
+        serializer = MySerializer(data={'url': 'http://testserver/path'})
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+
+    def test_url_validator(self):
+        validator = URLValidator()
+        self.assertIsNone(validator('http://testserver/path'))
