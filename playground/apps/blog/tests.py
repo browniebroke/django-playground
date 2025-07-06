18417d4003a1691c08829e9106cf4fab68889431
Bruno Alla <alla.brunoo@gmail.com>
2025-07-06 16:13:02 +0200

    Add test

--- playground/apps/blog/tests.py
+++ playground/apps/blog/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from playground.apps.blog.models import Post
+
+
+class BrowsableApiPaginationTests(TestCase):
+    def test_next_link(self):
+        Post.objects.bulk_create(
+            [
+                Post(title="Post 1", text="heelo"),
+                Post(title="Post 2", text="world"),
+                Post(title="Post 3", text="there"),
+                Post(title="Post 4", text="there"),
+            ]
+        )
+
+        response = self.client.get("/api/posts/?timestamp=3", headers={"Accept": "text/html"})
+
+        self.assertEqual(200, response.status_code)
+
+        self.assertContains(response, "Post 1")
+        self.assertContains(response, "Post 2")
+        self.assertNotContains(response, "Post 3")
+        self.assertContains(
+            response,
+            """<a href="http://testserver/api/posts/?page=2%C3%97tamp%3D3" rel="nofollow">http://testserver/api/posts/?page=2&amp;timestamp=3</a>""",
+            html=True
+        )
