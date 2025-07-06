707ff360fcbefae255ea6d3def6f858152ce1d8d
Bruno Alla <alla.brunoo@gmail.com>
2025-07-06 11:37:04 +0200

    Demo issue with escaped ampersand in browsable API:
    https://github.com/encode/django-rest-framework/issues/9643

--- playground/apps/blog/views.py
+++ playground/apps/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from playground.apps.blog.models import Post
+from playground.apps.blog.serializers import PostSerializer
+
+
+class SmallPageNumberPagination(PageNumberPagination):
+    page_size = 2
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_url_kwarg = "post_id"
+    pagination_class = SmallPageNumberPagination
--- playground/routers.py
+++ playground/routers.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
+from playground.apps.blog.views import PostViewSet
 
 router = DefaultRouter()
+router.register("posts", PostViewSet, basename="post")
 
 app_name = "api"
 urlpatterns = router.urls
